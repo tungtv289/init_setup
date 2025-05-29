49827a718da52a6f0a3ef3a94c9654c1b73bb6a3
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2025-05-29 21:51:42 +0700

    install font for lazyvim

--- macos/setup.sh
+++ macos/setup.sh
@@ -2,3 +2,9 @@ mkdir -p ~/utilities
 
 mv ./pwdx_for_mac.sh ~/utilities/pwdx_for_mac.sh
 mv ./clean_cache.sh ~/utilities/clean_cache.sh
+
+# install font for lazyvim
+mkdir -p ~/Downloads/nerd-fonts-temp && cd ~/Downloads/nerd-fonts-temp
+curl -LO https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/0xProto.zip
+unzip 0xProto.zip -d 0xProto
+sudo cp 0xProto/*.ttf ~/Library/Fonts
