3b73e9806879ba32a0cc77d57c78e3027e291a45
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2025-05-30 10:53:29 +0700

    sudo apt autoremove --pur

--- linux/setup.sh
+++ linux/setup.sh
@@ -13,3 +13,10 @@ git clone https://github.com/neovim/neovim.git
 git checkout v0.11.1
 make CMAKE_BUILD_TYPE=Release
 make CMAKE_BUILD_TYPE=Release
+
+
+# prune gnome
+sudo apt remove gnome-calendar gnome-characters gnome-sudoku gnome-mahjongg gnome-logs gnome-font-viewer gnome-getting-started-docs gnome-initial-setup gnome-todo
+sudo apt purge ubuntu-desktop ubuntu-gnome-desktop gnome-shell gnome-control-center gnome-session gnome-settings-daemon gnome-online-accounts gnome-power-manager gnome-shell-extension-desktop-icons
+sudo apt purge gedit totem nautilus gnome-calculator mousetweaks gnome-menus gnome-terminal
+sudo apt autoremove --pur
