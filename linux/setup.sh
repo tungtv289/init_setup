2f9873b30c497363c8f2e3e2c07483e01d652c66
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2025-05-30 09:38:13 +0700

    Setup nvim

--- linux/setup.sh
+++ linux/setup.sh
@@ -7,3 +7,9 @@ mv ./zshrc ~/.zshrc
 
 # setup terminator
 mv ./terminator_config ~/.config/terminator/config
+
+# setup nvim
+git clone https://github.com/neovim/neovim.git
+git checkout v0.11.1
+make CMAKE_BUILD_TYPE=Release
+make CMAKE_BUILD_TYPE=Release
