36af78eb8ba0cca63af43984e233f940a9eabb19
Tùng Trần <59041428+tungtv289@users.noreply.github.com>
2025-06-03 14:49:28 +0700

    Update setup.sh

--- linux/setup.sh
+++ linux/setup.sh
@@ -17,7 +17,5 @@ make CMAKE_BUILD_TYPE=Release
 
 # prune gnome
 sudo apt remove gnome-calendar gnome-characters gnome-sudoku gnome-mahjongg gnome-logs gnome-font-viewer gnome-getting-started-docs gnome-initial-setup gnome-todo
-sudo apt purge ubuntu-desktop ubuntu-gnome-desktop gnome-shell gnome-control-center gnome-session gnome-settings-daemon gnome-online-accounts gnome-power-manager gnome-shell-extension-desktop-icons
-sudo apt purge gedit totem nautilus gnome-calculator mousetweaks gnome-menus gnome-terminal
+sudo apt purge gedit totem gnome-calculator mousetweaks gnome-menus gnome-terminal
 sudo apt autoremove --purge
-sudo apt purge snapd
